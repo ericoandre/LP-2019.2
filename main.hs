c897130bab356fdfa8258380bfbeeafaef54a41b
Erico Andre <ericoandresilva@gmail.com>
2019-10-25 11:50:39 -0300

    Create main.hs

---
+++ main.hs
@@ -0,0 +1,15 @@
+multiplica x y  = x * y
+
+quadrado:: Int-> Int
+quadrado x = multiplica x x
+
+dobra = multiplica 2
+
+sqrInt::Int-> Int
+sqrInt x = x * x
+
+sqrInteger:: Integer -> Integer
+sqrInteger x = multiplica x x
+
+main = do
+putStrLn "Erico Andre"
